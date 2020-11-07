e84f632cf70dff98482cb8e21605cba0ed343809
Alvaro Morales <Ag1mor@berkeley.edu>
2020-11-07 13:25:21 -0800

    Added Navigation Page for Development
    Used 'react-router-dom' to create a temporary home page for access
    to all other pages in the app. Also decided to add reactstrap to
    get basic components for development.

---
+++ edifi/src/pages/ProfilePage.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+function ProfilePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is the Profile Page!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default ProfilePage;
