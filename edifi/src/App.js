571be52d0298ffcc001c16a654db65810bb75be8
Alvaro Morales <Ag1mor@berkeley.edu>
2020-11-07 13:25:21 -0800

    Added Navigation Page for Development
    Used 'react-router-dom' to create a temporary home page for access
    to all other pages in the app. Also decided to add reactstrap to
    get basic components for development.

--- edifi/src/App.js
+++ edifi/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SearchPage from './pages/SearchPage'
+import NavigationPage from './pages/NavigationPage'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Switch>
+            <Route path="/" exact component={NavigationPage} />
+            <Route path="/Login" exact component={LoginPage} />
+            <Route path="/Profile" exact component={ProfilePage} />
+            <Route path="/Search" exact component={SearchPage} />
+          </Switch>
+    </Router>
   );
 }
 
