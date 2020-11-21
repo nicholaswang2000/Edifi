8d47bd9831f40487a09e807c87ea65ae8a7ac624
Shaina <shaina.sierra@gmail.com>
2020-11-21 11:29:43 -0800

    Update with test write to speaker collection

--- edifi/src/backend/app.py
+++ edifi/src/backend/app.py
@@ -1,9 +1,11 @@
 from database.MongoDatabase import MongoDatabase
 from data.Educator import Educator
+from data.Speaker import Speaker
 
 MongoDatabase.init()
 
 educator = Educator("Ben Smith", [1, 2, 3], ["science", "math"], "bensmith@test.org")
-
 MongoDatabase.insert('educator', educator.convertToDocument())
 
+speaker = Speaker("Sally Smith", "twitter", "content manager", "sallysmith@test.org")
+MongoDatabase.insert('speaker', speaker.convertToDocument())
